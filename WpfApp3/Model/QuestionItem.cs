fdb5a32e0bddd87e733d8261583b0f78f19d1877
Lucky Chandrautama <chandrautama@outlook.de>
2025-05-12 00:38:32 +0200

    Add project files.

---
+++ WpfApp3/Model/QuestionItem.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+
+public class QuestionItem
+{
+
+    private int id;
+
+    private DateTime date;
+
+    private string question;
+
+    private string[] choices;
+
+    private int solution;
+
+    public QuestionItem(int id, DateTime date, string question, string[] choices, int solution)
+    {
+        this.id = id;
+        this.date = date;
+        this.question = question;
+        this.choices = choices;
+        this.solution = solution;
+
+    }
+
+    public int Id { get => id; set => id = value; }
+    public DateTime Date { get => date; set => date = value; }
+    public string Question { get => question; set => question = value; }
+    public string[] Choices { get => choices; set => choices = value; }
+    public int Solution { get => solution; set => solution = value; }
+
+    public List<string> ChoicesAsList()
+    {
+        return [.. choices];
+    }
+}
---
+++ WpfApp3/ViewModel/ContentViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using WpfApp3.MVVM;
+
+
+namespace WpfApp3.ViewModel
+{
+    internal class ContentViewModel:ViewModelBase
+    {
+        public ObservableCollection<QuestionItem> QuestionItemsCatalogue { get; set; }
+
+        public ContentViewModel() {
+
+            QuestionItemsCatalogue = new ObservableCollection<QuestionItem>();
+
+            string[] strings = ["It enhances security among users", "It minimizes interference and encourage collaboration"];
+            QuestionItem questionItem = new QuestionItem(id: 25, date: DateTime.Now, question: "Why is it important to define teritoriality in social VR?", choices: strings, solution:1);
+
+            QuestionItemsCatalogue.Add(questionItem);
+        }
+
+        private QuestionItem selectedQuestionItem;
+
+        public QuestionItem SelectedQuestionItem
+        {
+            get { return selectedQuestionItem; }
+            set { selectedQuestionItem = value; OnPropertyChanged(); }
+        }
+
+    }
+}
+
